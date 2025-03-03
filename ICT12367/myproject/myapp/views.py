7ba464be8eb768ee87325826844247be4d745d05
Chatchawan Worawitrattanakul <chatchawan.wo@gmail.com>
2025-03-04 02:29:54 +0700

    URL & View

---
+++ ICT12367/myproject/myapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("<h1>Hello, world. You're at the polls index.</h1>")
+
+def about(request):
+    return HttpResponse("<h1>This is the about page</h1>")
+
+def form(request):
+    return render(request, 'form.html')
+
+def contact(request):
+    return HttpResponse("<h1>ติดต่อ ผู้สอน</H1>") 
